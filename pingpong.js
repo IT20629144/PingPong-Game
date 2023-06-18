ad9f856694c40ae344d5594c8b6ceaf7beb22848
Ravindu Nirmal <ravindu.nirmal099@gmail.com>
2023-06-18 13:23:07 +0530

    PaddlesCreated

--- pingpong.js
+++ pingpong.js
@@ -1,7 +1,7 @@
-const gameBoard = document.querySelector("gameBoard");
+const gameBoard = document.querySelector("#gameBoard");
 const ctx = gameBoard.getContext("2d");
-const scoreText = document.querySelector("scoreText");
-const restartButton = document.querySelector("restartButton");
+const scoreText = document.querySelector("#scoreText");
+const restartButton = document.querySelector("#restartButton");
 const gameWidth = gameBoard.width;
 const gameHeight = gameBoard.height;
 const backgroundColor = "forestgreen";
@@ -33,23 +33,50 @@ let paddle2 = {
     y:gameHeight - 100
 };
 
-window.addEventListener("keydown", changeDirection);
-restartButton.addEventListener("click", restartGame);
+// window.addEventListener("keydown", changeDirection);
+// restartButton.addEventListener("click", restartGame);
 
 gameStart();
 
 function gameStart(){
-    createBall();
+    // createBall();
     nextTick();
+    console.log("hello");
+
 }
 
 function nextTick(){
     intervalID = setTimeout(() =>{
         clearBoard();
         drawPaddles();
-        moveBall();
-        drawBall();
-        checkCollision();
+        // moveBall();
+        // drawBall();
+        // checkCollision();
         nextTick();
     },10);
 }
+
+function clearBoard(){
+    ctx.fillStyle = backgroundColor;
+    ctx.fillRect(0,0,gameWidth,gameHeight);
+}
+
+function drawPaddles(){
+    ctx.strokeStyle = paddleBorder;
+
+    ctx.fillStyle = paddle1Color;
+    ctx.fillRect(paddle1.x,paddle1.y,paddle1.width,paddle1.height);
+    ctx.strokeRect(paddle1.x,paddle1.y,paddle1.width,paddle1.height);
+
+    ctx.fillStyle = paddle2Color;
+    ctx.fillRect(paddle2.x,paddle2.y,paddle2.width,paddle2.height);
+    ctx.strokeRect(paddle2.x,paddle2.y,paddle2.width,paddle2.height);
+};
+// function moveBall(){};
+// function drawBall(){};
+// function checkCollision(){};
+// function nextTick(){};
+
+// function createBall(){};
+// function changeDirection(){};
+// function restartGame(){};
